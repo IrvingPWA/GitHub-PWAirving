031b4ac77a50982f0742343cf8a507f03822fa27
IrvingPWA <82490186+IrvingPWA@users.noreply.github.com>
2021-04-14 02:59:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  * cambios en tu sitio, para
  * depués actualizar este archivo.
  */
-const CACHE = "irvingpwa-1.01";
+const CACHE = "irvingpwa-1.10";
 
 /** Archivos requeridos para que
  * la aplicación funcione fuera de
